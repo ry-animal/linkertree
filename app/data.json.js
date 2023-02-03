933acbad0ee9793480e683da2383ac74ef9dd469
sc-ryanv2 <ryan@snowcrash.com>
2023-02-03 14:42:17 -0800

    Add linkertree with edge config

---
+++ app/data.json.js
@@ -0,0 +1,30 @@
+// USE THIS TO GET THE JSON DATA FOR YOUR PROFILE IN EDGE CONFIG
+
+// {
+//   "name": "ryanimal",
+//   "avatar": "https://pbs.twimg.com/profile_images/1507509362998210561/JqBECkA2_400x400.jpg",
+//   "links": [
+//     {
+//       "title": "My Website",
+//       "href": "https://twitter.com/ryanimal",
+//       "image": "https://pbs.twimg.com/profile_images/1507509362998210561/JqBECkA2_400x400.jpg"
+//     },
+//     {
+//       "title": "Snowcrash.com",
+//       "href": "https://snowcrash.com/",
+//       "image": "https://pbs.twimg.com/profile_images/1507509362998210561/JqBECkA2_400x400.jpg"
+//     }
+//   ],
+//   "socials": [
+//     {
+//       "name": "twitter",
+//       "href": "https://twitter.com/ryanimal",
+//       "image": "https://pbs.twimg.com/profile_images/1507509362998210561/JqBECkA2_400x400.jpg"
+//     },
+//     {
+//       "name": "github",
+//       "href": "https://twitter.com/ryanimal",
+//       "image": "https://pbs.twimg.com/profile_images/1507509362998210561/JqBECkA2_400x400.jpg"
+//     }
+//   ]
+// }
